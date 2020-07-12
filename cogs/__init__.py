df2d685dea48cafc479e41e627c1be4751ff8ce6
vcokltfre <vcokltfre@pop-os.localdomain>
2020-07-12 05:24:05 +0100

    feat: add base encode module

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
 
-from .general import general, stats
+from .general import general, stats, encode
 
 def setup(bot: commands.Bot):
     bot.add_cog(general.General(bot))
     bot.add_cog(stats.Stats(bot))
+    bot.add_cog(encode.Encode(bot))
